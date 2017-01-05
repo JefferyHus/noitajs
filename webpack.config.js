b0bce2a17be4713d9335fbf37c829c8c7f7edd01
JefferyHus <jefferytutorials@gmail.com>
2017-01-05 21:02:35 +0000

    travis.ci

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+{
+    const webpack = require("webpack");
+    const path = require("path");
+
+    module.exports = {
+        entry: {
+            js: './noita-dev.ts'
+        },
+        output: {
+            path: './dist',
+            filename: 'noita.js'
+        },
+        module: {
+            loaders: [
+                {test: /\.ts$/, loader: 'ts-loader', exclude: [/node_modules/, /typings/]},
+                {test: /\.js$/, loader: 'babel-loader', exclude: [/node_modules/, /typings/]}
+            ]
+        },
+        resolve: {
+            extensions: ['', '.js', '.ts']
+        }
+    }
+}
