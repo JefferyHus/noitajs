80cf0aef74fcf28b183edff648d1c032baf31bc9
jeffery hussin <jefferytutorials@gmail.com>
2017-01-05 18:10:27 +0000

    lodash upgraded, unit testing performance

--- noita-dev.ts
+++ noita-dev.ts
@@ -1,29 +1,24 @@
 import _ from "lodash";
 
 // create a new class
-class noita {
+export class noita {
     // set the default variables
-    repeat = false;
-    repeatDelay = 300;
-    function = 'swing';
-    duration = 300;
-    timing = 'ms';
-    properties = {};
-    init = (elem) => {};
-    complete = (settings, error) => {};
+    settings: _sobject;
 
     constructor (options) {
-        console.log(options);
+        _.extend(this.settings, options);
     }
 
 }
 
-/*repeat: false,
-repeatDelay: 300,
-repeatProps: {},
-function: 'swing',
-duration: 300,
-timing: 'ms',
-properties: {},
-init: function (elem) {},
-complete: function (settings, error) {}*/
+// separate the settings object for a clean changes
+interface _sobject{
+    repeat: false;
+    repeatDelay: 300;
+    function: 'swing';
+    duration: 300;
+    timing: 'ms';
+    properties: {};
+    init: (elem) => {};
+    complete: (settings, error) => {};
+}
